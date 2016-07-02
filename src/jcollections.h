c1c7a3710886e1f5f7d119b6b0fce762d03cbe4b
jacopodl <jacopo.delu@gmail.com>
2016-07-02 21:35:31 +0200

    Added LGPL V3

--- src/jcollections.h
+++ src/jcollections.h
@@ -1,12 +1,27 @@
-//
-// Created by jdl on 29/06/16.
-//
+/*
+ * JCollections: A small generic collections library for C language.
+ * Copyright (C) 2014-2016 Jacopo De Luca
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef JCOLLECTIONS_H
 #define JCOLLECTIONS_H
 
 #define JCOLLECTIONS_VERSION_MAJOR  @VERSION_MAJOR@
 #define JCOLLECTIONS_VERSION_MINOR  @VERSION_MINOR@
+#define JCOLLECTIONS_VERSION_PATCH  @VERSION_PATCH@
 
 #include "JCdatatype.h"
 #include "utils.h"
--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,20 @@
-//
-// Created by jdl on 29/06/16.
-//
+/*
+* utils, part of JCollections.
+* Copyright (C) 2014-2016 Jacopo De Luca
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 3 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef JCOLLECTIONS_UTILS_H
 #define JCOLLECTIONS_UTILS_H
